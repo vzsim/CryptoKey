9dc7ec208d8a796d0069f62ee02aebd28bb7eb0d
Telman Islamov <it@tislamov.ru>
2025-06-07 12:23:14 +0300

    Implemented CHANGE REFERENCE DATA and VERIFY.
    Also added support for DEACTIVATED and TERMINATED life cycle states

---
+++ src/vzsim/CryptoKey.java
@@ -0,0 +1,310 @@
+package vzsim;
+
+import javacard.framework.APDU;
+import javacard.framework.Applet;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.OwnerPIN;
+
+public class CryptoKey extends Applet implements ISO7816
+{
+	/* Constant values */
+	private static final byte INS_VERIFY                = (byte) 0x20;
+    private static final byte INS_CHANGE_REFERENCE_DATA = (byte) 0x25;
+	private static final byte INS_RESET_RETRY_COUNTER   = (byte) 0x2D;
+
+	private static final short SW_PIN_TRIES_REMAINING   = 0x63C0; // See ISO 7816-4 section 7.5.1
+
+	private static final boolean PUK_MUST_BE_SET        = false;
+	private static final byte PIN_MAX_TRIES             = (byte) 0x03;
+	private static final byte PUK_MAX_TRIES             = (byte) 0x0A;
+	private static final byte PIN_MIN_LENGTH            = (byte) 0x04;
+	private static final byte PIN_MAX_LENGTH            = (byte) 0x10;
+	
+	/** No restrictions */
+	private static final byte APP_STATE_CREATION        = (byte) 0x00;
+	
+	/** PUK set, but PIN not set yet. */
+	private static final byte APP_STATE_INITIALIZATION  = (byte) 0x01;
+
+	/** PIN is set. data is secured. */
+	private static final byte APP_STATE_ACTIVATED       = (byte) 0x05;
+
+	/** Applet usage is deactivated. */
+	private static final byte APP_STATE_DEACTIVATED     = (byte) 0x04;
+
+	/** Applet usage is terminated. */
+	private static final byte APP_STATE_TERMINATED      = (byte) 0x0C;
+	
+	
+
+
+	private byte appletState;
+	private OwnerPIN pin = null;
+	private OwnerPIN puk = null;
+	
+	public
+	CryptoKey()
+	{
+		pin = new OwnerPIN(PIN_MAX_TRIES, PIN_MAX_LENGTH);
+		appletState = APP_STATE_CREATION;
+	}
+
+	public static void
+	install(byte[] bArray, short bOffset, byte bLength)
+	{
+		CryptoKey temp = new CryptoKey();
+		temp.register();
+	}
+	
+	public void
+	process(APDU apdu) throws ISOException
+	{
+		if (selectingApplet()) {
+			return;
+		}
+
+		byte[] buff = apdu.getBuffer();
+		byte ins = buff[OFFSET_INS];
+
+		if (appletState == APP_STATE_TERMINATED) {
+			ISOException.throwIt((short)(SW_UNKNOWN | APP_STATE_TERMINATED));
+		}
+
+		switch (ins) {
+			case INS_CHANGE_REFERENCE_DATA: {
+				changeReferenceData(apdu);
+			} break;
+			case INS_VERIFY: {
+				verify(apdu);
+			} break;
+			case INS_RESET_RETRY_COUNTER: {
+				resetRetryCounter(apdu);
+			} break;
+			default: {
+				ISOException.throwIt(SW_INS_NOT_SUPPORTED);
+			}
+		}
+	}
+
+	/**
+	 * CHANGE REFERENCE DATA (INS 0X25), ISO 7816-4, clause 11.5.7.
+	 * 
+	 * CDATA shall contain BER-TLV data object (ISO 7816-4, clause 6.3) to make it possible to
+	 * distinguish verification data (current PIN) from new reference data (new PIN). Thus the content
+	 * of CDATA at APP_STATE_ACTIVATED state (e.g. updating PIN) shall be as follow: [81, Len, CURR_PIN, 82, Len, NEW_PIN]
+	 * @param apdu
+	 */
+	private void
+	changeReferenceData(APDU apdu)
+	{
+		byte[] buff = apdu.getBuffer();
+		byte p1 = buff[OFFSET_P1];
+		byte p2 = buff[OFFSET_P2];
+
+		short cdataOff, lc, len = 0, off = 0;
+
+		lc = apdu.setIncomingAndReceive();
+		if (lc == (short)0 || lc != apdu.getIncomingLength()) {
+			ISOException.throwIt(SW_WRONG_LENGTH);
+		}
+
+		cdataOff = apdu.getOffsetCdata();
+
+		len = UtilTLV.tlvGetLen(buff, cdataOff, lc, (byte)0x81);
+		off = UtilTLV.tlvGetValue(buff, cdataOff, lc, (byte)0x81);
+
+		if (len < PIN_MIN_LENGTH || len > PIN_MAX_LENGTH || off == (short)-1) {
+			ISOException.throwIt(SW_WRONG_DATA);
+		}
+		
+		switch (appletState) {
+
+			case APP_STATE_CREATION: {	// Set either PIN or PUK
+
+				if (p1 != (byte)0x01 || (p2 != (byte)0x01 && p2 != (byte)0x02)) {
+					ISOException.throwIt(SW_INCORRECT_P1P2);
+				}
+
+				/* Set the PIN and move to APP_STATE_ACTIVATED. in this case no PUK will be set, ever.  */
+				if (p2 == (byte)0x01) {
+
+					if (PUK_MUST_BE_SET) {
+						ISOException.throwIt(SW_COMMAND_NOT_ALLOWED);
+					}
+
+					pin.update(buff, off, (byte)len);
+					pin.resetAndUnblock();
+
+					appletState = APP_STATE_ACTIVATED;
+
+				// Setting the PUK and move to APP_STATE_INITIALIZATION
+				} else {
+					puk = new OwnerPIN(PUK_MAX_TRIES, PIN_MAX_LENGTH);
+					puk.update(buff, off, (byte)len);
+					puk.resetAndUnblock();
+
+					appletState = APP_STATE_INITIALIZATION;
+				}
+			} break;
+			case APP_STATE_INITIALIZATION: {	// Set PIN
+
+				if (p1 != (byte)0x01 || p2 != (byte)0x01) {
+					ISOException.throwIt(SW_INCORRECT_P1P2);
+				}
+
+				pin.update(buff, off, (byte)len);
+				pin.resetAndUnblock();
+
+				appletState = APP_STATE_ACTIVATED;
+				
+			} break;
+			case APP_STATE_ACTIVATED: {	// Update PIN
+
+				if (p1 != (byte)0x00 || p2 != (byte)0x00) {
+					ISOException.throwIt(SW_INCORRECT_P1P2);
+				}
+
+				// Check the old PIN
+				if (!pin.check(buff, off, (byte)len)) {
+					ISOException.throwIt((short)(SW_PIN_TRIES_REMAINING | (short)0x000C));
+				}
+
+				len = UtilTLV.tlvGetLen(buff, cdataOff, lc, (byte)0x82);
+				off = UtilTLV.tlvGetValue(buff, cdataOff, lc, (byte)0x82);
+
+				if (len < PIN_MIN_LENGTH || len > PIN_MAX_LENGTH || off == (short)-1) {
+					ISOException.throwIt(SW_WRONG_DATA);
+				}
+
+				// Update PIN
+				pin.update(buff, off, (byte)len);
+				pin.resetAndUnblock();
+
+			} break;
+			default: ISOException.throwIt(SW_COMMAND_NOT_ALLOWED);
+		}
+	}
+
+	/**
+	 * VERIFY (INS 0X20), ISO 7816-4, clause 11.5.6.
+	 * @param apdu
+	 */
+	private void
+	verify(APDU apdu)
+	{
+		byte[] buff = apdu.getBuffer();
+		byte p1 = buff[OFFSET_P1];
+		byte p2 = buff[OFFSET_P2];
+
+		short cdataOff, lc;
+
+		if (appletState == APP_STATE_DEACTIVATED) {
+			ISOException.throwIt(SW_COMMAND_NOT_ALLOWED);
+		}
+
+		if (p1 != (byte)0x00 || p2 != (byte)0x01) {
+			ISOException.throwIt(SW_INCORRECT_P1P2);
+		}
+		
+		lc = apdu.setIncomingAndReceive();
+		if (lc != apdu.getIncomingLength())  {
+			ISOException.throwIt(SW_WRONG_LENGTH);
+		}
+		
+		cdataOff = apdu.getOffsetCdata();
+
+		// At the below mentioned states no PIN is set yet, thus there is no error at all.
+		if (lc == (byte)0x00 && (appletState == APP_STATE_CREATION || appletState == APP_STATE_INITIALIZATION)) {
+			
+			ISOException.throwIt(SW_NO_ERROR);
+
+		} else if (lc == (byte)0x00 && (appletState == APP_STATE_ACTIVATED)) {
+
+			// Absence of CDATA means that user requested the number of remaining tries.
+			ISOException.throwIt((short)(SW_PIN_TRIES_REMAINING | pin.getTriesRemaining()));
+		}
+
+		if (lc < PIN_MIN_LENGTH || lc > PIN_MAX_LENGTH) {
+			ISOException.throwIt(SW_WRONG_LENGTH);
+		}
+
+		// Check the PIN.
+		if (!pin.check(buff, cdataOff, (byte)lc)) {
+
+			if (pin.getTriesRemaining() < (byte)1) {
+				appletState = APP_STATE_DEACTIVATED;
+			}
+
+			ISOException.throwIt((short)(SW_PIN_TRIES_REMAINING | pin.getTriesRemaining()));
+		}
+	}
+
+	/**
+	 * RESET RETRY COUNTER (INS 0X2D), ISO 7816-4, clause 11.5.10.
+	 * Supported combinations are:
+	 * P1 == 0 CDATA: [81 Len PUK && 82 Len NEW PIN] // appying new PIN
+	 * P1 == 1 CDATA: [81 Len PUK]					 // Just reset PIN tries counter
+	 * P3 == 3 CDATA: absent						 // get PUK remaining tries
+	 * @param apdu
+	 */
+	private void
+	resetRetryCounter(APDU apdu)
+	{
+		byte[] buff = apdu.getBuffer();
+		byte p1 = buff[OFFSET_P1];
+		byte p2 = buff[OFFSET_P2];
+		short cdataOff, lc, len, off;
+
+		if (appletState != APP_STATE_DEACTIVATED || puk == null) {
+			ISOException.throwIt(SW_COMMAND_NOT_ALLOWED);
+		}
+
+		if ((p1 == (byte)0x02 || p1 > (byte)0x03) || p2 != (byte)0x01) {
+			ISOException.throwIt(SW_INCORRECT_P1P2);
+		}
+
+		lc = apdu.setIncomingAndReceive();
+		if (lc != apdu.getIncomingLength()) {
+			ISOException.throwIt(SW_WRONG_LENGTH);
+		}
+		cdataOff = apdu.getOffsetCdata();
+
+		// User requested PUK tries counter only.
+		if (p1 == (byte)0x03) {
+			ISOException.throwIt((short)(SW_PIN_TRIES_REMAINING | puk.getTriesRemaining()));
+		}
+
+		// Common case for P1=0 and P1=1: retrieving PUK
+		len = UtilTLV.tlvGetLen(buff, cdataOff, lc, (byte)0x81);
+		off = UtilTLV.tlvGetValue(buff, cdataOff, lc, (byte)0x81);
+
+		if (len < PIN_MIN_LENGTH || len > PIN_MAX_LENGTH || off == (short)-1) {
+			ISOException.throwIt(SW_WRONG_DATA);
+		}
+
+		if (!puk.check(buff, off, (byte)len)) {
+			if (puk.getTriesRemaining() < (byte)1) {
+				appletState = APP_STATE_TERMINATED;
+			}
+			ISOException.throwIt((short)(SW_PIN_TRIES_REMAINING | puk.getTriesRemaining()));
+		}
+
+		// P1=0: retrieve and apply a new PIN value.
+		if (p1 == (byte)0x00) {
+			
+			off = UtilTLV.tlvGetValue(buff, cdataOff, lc, (byte)0x82);
+			len = UtilTLV.tlvGetLen(buff, cdataOff, lc, (byte)0x82);
+
+			if (len < PIN_MIN_LENGTH || len > PIN_MAX_LENGTH || off == (short)-1) {
+				ISOException.throwIt(SW_WRONG_DATA);
+			}
+
+			pin.update(buff, off, (byte)len);
+		}
+
+		// Committing commmon case for P1=0 and P1=1: reset and unblock PIN
+		pin.resetAndUnblock();
+		appletState = APP_STATE_ACTIVATED;
+	}
+}
